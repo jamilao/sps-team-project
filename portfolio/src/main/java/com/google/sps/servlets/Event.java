981d3fbc999af2c60a94557caafe6a9abf854557
Ness Chu <vanessa.chu@mail.mcgill.ca>
2020-07-16 17:54:36 -0400

    Add function to Event.java to generate event passwords.

--- portfolio/src/main/java/com/google/sps/servlets/Event.java
+++ portfolio/src/main/java/com/google/sps/servlets/Event.java
@@ -7,6 +7,7 @@ public final class Event{
     private final String location;
     private final String description;
     private final Date date;
+    private final String password;
 
     public Event(String organizer, String eventName, String location, String description, Date date){
         this.organizer = organizer;
@@ -14,17 +15,34 @@ public final class Event{
         this.location = location;
         this.description = description;
         this.date = date;
+        this.password = generatePassword();
+    }
+
+    private String generatePassword(){
+        String password = "";
+        for (int i = 0; i < 5; i++){
+            char rand = (char)((int)(Math.random() * 97) + 33); 
+            password = password + rand;
+        }
+        return password;
+    }
+
+    public String getPassword(){
+        return password;
     }
 
     public String getOrganizer(){
        return organizer;
     }
+    
     public String getEventName(){
        return eventName;
     }
+    
     public String getLocation(){
        return location;
     }
+    
     public String getDescription(){
        return description;
     }
