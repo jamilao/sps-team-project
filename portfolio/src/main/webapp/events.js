2ce8bcd757d9888874cd926be3ffa379a49a3c67
Ness Chu <vanessa.chu@mail.mcgill.ca>
2020-07-20 00:37:52 -0400

    Add password form redirecting to event edit on single event pages. Some subfeatures still broken.

--- portfolio/src/main/webapp/events.js
+++ portfolio/src/main/webapp/events.js
@@ -35,6 +35,39 @@ async function fetchEvents(){
         });
     });
 }
+
+// This method successfully checks a password and displays the edit page.
+// TO-DO: Fix bug where the first event loaded/clicked in events.html has its data persis (i.e. password, displayed info).
+function checkPassword(){
+    const Http = new XMLHttpRequest();
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    const url = "/event?id=" + id;
+    var event = null;
+    Http.responseType = 'json';
+    Http.open("GET", url);
+    Http.send();
+    Http.onreadystatechange = function(){
+        if(this.readyState==4 && this.status==200){
+            event = (Http.response)[0];
+            var password = document.getElementById('password').value;
+            console.log(password);
+            console.log(event.password);
+            if (password === event.password){
+                document.getElementById('main').innerHTML='<object style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; width: 100%; height: 100%;" type="text/html" data="organizereventedit.html"></object>';
+                // TO-DO: The following do not successfully fill in the values, possibly due to the main innerHTML composing of an object.
+                document.getElementById('eventName').value=event.eventName;
+                document.getElementById('location').value=event.location;
+                document.getElementById('start').value=event.start;
+                document.getElementById('end').value=event.end;
+                document.getElementById('description').value=event.description;
+            }
+            else{
+                alert("Incorrect password.");
+            }
+        }
+    }
+}
 function displayEvent(){
     console.log("Calling displayEvent()");
     const Http = new XMLHttpRequest();
@@ -71,7 +104,10 @@ function displayEvent(){
             start.innerText = "When: " + event.start;
             newDiv.append(start);
             container.append(newDiv);
+            var passwordDiv = document.createElement("div");
+            passwordDiv.innerHTML = "<input type='text' id='password' placeholder='Password'/><button type='button' onclick='checkPassword()'>Edit Event</button>";
+            container.append(passwordDiv);
             console.log("Event added");
         }
-    }  
+    }
 }
