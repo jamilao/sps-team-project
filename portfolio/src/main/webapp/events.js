81187317dfd422c2985831f1d5f5535baf0db5a1
Eric Wu <ewu48@uwo.ca>
2020-07-24 14:47:32 -0400

    Basic Filtering of Events

--- portfolio/src/main/webapp/events.js
+++ portfolio/src/main/webapp/events.js
@@ -8,6 +8,40 @@ async function fetchEvents(){
         console.log(events);
         events.forEach((event)=> {
             console.log(event);
+
+            if(document.getElementById("cities").value != "All"){
+            if(document.getElementById("cities").value == event.location){
+                 const eventID = event.key.id.toString();
+            var newDiv = document.createElement("div");
+            newDiv.setAttribute("class","child");
+            newDiv.setAttribute("id",eventID);
+            var imgContainer = document.createElement("div");   
+            imgContainer.setAttribute("class","img-container");
+            var image = document.createElement("img");
+            image.setAttribute("src","/images/imgicon.png");
+            imgContainer.append(image);
+            newDiv.append(imgContainer);
+            imgContainer.onclick = function(){      
+                window.location.href = "/event.html?id=" + eventID;
+            }
+            var name = document.createElement("h4");
+            name.innerText = event.eventName;
+            newDiv.append(name);
+            var location = document.createElement("h4");
+            location.innerText = "Location: " + event.location;
+            newDiv.append(location);
+            var start = document.createElement("h4");
+            start.innerText = "When: " + event.start;
+            newDiv.append(start);
+            container.append(newDiv);
+            console.log("Event added");
+            }
+            else{
+               
+            }  
+            } 
+            if(document.getElementById("cities").value == "All"){
+            
             const eventID = event.key.id.toString();
             var newDiv = document.createElement("div");
             newDiv.setAttribute("class","child");
@@ -32,6 +66,8 @@ async function fetchEvents(){
             newDiv.append(start);
             container.append(newDiv);
             console.log("Event added");
+            }
+            
         });
     });
 }
